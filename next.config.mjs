a9594449d80a62d33bba4d348882219e9c7da82f
hallykoppter <officialkurniasandi@gmail.com>
2025-05-28 13:02:31 +0700

    next config webpack monorepojs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin/index"
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin/index.js"
 
 const nextConfig = {
   allowedDevOrigins: ["skl.smpn3rancah.my.id"],
